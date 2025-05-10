f489baba407eef79742b96197e0b74dceeb438e2
NgoQuocHieu-UTH <ngoquochieu.scf@gmail.com>
2025-05-10 08:51:07 +0700

    feat:setup backend with node.js + ts

---
+++ backend/dist/server.js
@@ -0,0 +1,12 @@
+import dotenv from "dotenv";
+import express from "express";
+const app = express();
+dotenv.config();
+const port = process.env.PORT;
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+app.listen(port, () => {
+    return console.log(`Express is listening at http://localhost:${port}`);
+});
+//# sourceMappingURL=server.js.map
